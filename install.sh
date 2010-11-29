2436f31eb8ebfd9a1e95ae5d461ae22d765da8f9
kucaahbe <kucaahbe@ukr.net>
2010-11-29 13:13:25 +0200

    modified install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ for f in `ls -1`
 do
   if [ "./$f" != $0 ]
   then
-    ln -s -f $PWD/$f ~/.$f
+    cp -f $PWD/$f ~/.$f
   fi
 done
+
+echo "Enter username:"
+read name
+echo "Enter email:"
+read email
+
+git config --global user.name "$name"
+git config --global user.email $email
